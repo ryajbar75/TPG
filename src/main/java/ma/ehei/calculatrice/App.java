097a750b91416cd73d006b39d2787d318b53da0b
YAJBAR rafik <rafikyajbar75@gmail.com>
2024-01-04 00:13:06 +0100

    Ajoute et appele multip

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -4,4 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
 	}
+	public static void main(String[] args) {
+		System.out.println(Calculatrice.mul(2, 2));
+	}
 }
